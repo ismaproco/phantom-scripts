dd9e094a246a0c6e844f0943bddc2b3bb14f6e73
Ismael <ismaproco@gmail.com>
2017-02-05 14:18:25 +0100

    added verbosity to the spec

--- specs/url2img-phantom.spec.js
+++ specs/url2img-phantom.spec.js
@@ -1,2 +1,19 @@
 var u2i = require('../exports/url2img-phantom');
-console.log( u2i.phantomUrl2Img('http://www.google.com', 'google.png') );
+
+const URL = 'http://www.maxhine.com';
+const IMAGE_PATH = `${Date.now()}-bg.png`;
+
+var u2i$ = u2i.phantomUrl2Img( URL, IMAGE_PATH);
+
+// wait for the promise to complete
+u2i$.then((resultObj) => {
+    if(resultObj.error) {
+        console.log('error');
+    } else if( resultObj.pageStatus === 'fail') {
+        console.log('cannot get url');
+    } else if( !resultObj.renderStatus ) {
+        console.log('error generating image');
+    } else {
+        console.log( 'finish!',resultObj );    
+    }
+});
