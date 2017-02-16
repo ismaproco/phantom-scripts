77da8121ef0487106cd5fb9fec4969b41730c802
Ismael <ismaproco@gmail.com>
2017-02-16 23:16:07 +0100

    Improved page flow and styles

--- connectors/static/app.js
+++ connectors/static/app.js
@@ -1,33 +1,58 @@
 function addImageToPage(src) {
+  var imgContainer = document.querySelector('.img-container');
   var img = document.createElement("img");
   img.src = src.split('/')[2];
-
-  var container = document.querySelector('.img-container');
-  var content = document.querySelector('.content');
-
-  content.style.display = 'none';
-  container.appendChild(img);
+  imgContainer.appendChild(img);
 }
 
 function getImage(_captchaResponse, _url) {
-  fetch('http://localhost:3000/', {
-    method: 'POST',
-    mode: 'cors',
-    redirect: 'follow',
-    headers: new Headers({
-      'Content-Type': 'application/json'
-    }),
-    body: JSON.stringify({
-      recaptcha: _captchaResponse,
-      url: _url
-    })
-  }).then(function (response) {
-    console.log('external response', response);
-    return response.json();
-  }).then(function (json) {
-    console.log('json -> ', json);
-    addImageToPage(json.imgSrc);
-  });
+  setTimeout(function(){
+    containersVisibilityByState( 'loading' );
+      fetch('http://localhost:3000/', {
+        method: 'POST',
+        mode: 'cors',
+        redirect: 'follow',
+        headers: new Headers({
+          'Content-Type': 'application/json'
+        }),
+        body: JSON.stringify({
+          recaptcha: _captchaResponse,
+          url: _url
+        })
+      }).then(function (response) {
+        console.log('external response', response);
+        return response.json();
+      }).then(function (json) {
+        console.log('json -> ', json);
+        addImageToPage(json.imgSrc);
+        containersVisibilityByState( 'image' );
+      });
+  },1000);
+}
+
+function containersVisibilityByState(state) {
+  var imgContainer = document.querySelector('.img-container');
+  var content = document.querySelector('.content');
+  var loadingContent = document.querySelector('.loading-content');
+
+  content.style.display = 'none';
+  imgContainer.style.display = 'none';
+  loadingContent.style.display = 'none';
+
+  switch(state) {
+    case 'init':
+      content.style.display = 'block';
+    break;
+    case 'image':
+      imgContainer.style.display = 'flex';
+    break;
+    case 'loading':
+      loadingContent.style.display = 'block';
+    break;
+    default:
+
+    break;
+  }
 }
 
 
